c28dca2ee9edd41a0c87b4737a1503be11747fb6
azardilis <azardilis@gmail.com>
2014-08-13 01:13:46 +0100

    removed unused functions. added comments

--- ml_est.R
+++ ml_est.R
@@ -1,3 +1,5 @@
+library("truncnorm")
+
 MakeAbsNums <- function(d) {
     nd <- apply(d, 1, function(x) { ceiling(x / min(x[x > 0])) })
 
@@ -8,7 +10,6 @@ GetMLEstimates <- function(data) {
     # Return point estimates for the parameters based on the ML
     # estimation of success probabilities of the independent
     # Bernoulli trials
-
 
     n.data <- MakeAbsNums(data)
     n.succ <- colSums(n.data)
@@ -34,25 +35,8 @@ FilterControls <- function() {
     return(data)
 }
 
-GetMLEstimatesSample <- function() {
-    #Return point estimates for the parameters based on the ML
-    #estimation of success probabilities per sample
-    fname <- "data/GCFIDrawdata.csv"
-
-    data <- read.csv(fname)
-    fas <- c("C.12.0", "C.14.0", "C.16.0", "C.18.0", "C.20.0", "C22.0")
-    data <- data[, rev(fas)]
-
-    res <- apply(data, 1, function(x) {GetMLEstimates(matrix(x, nrow=1))})
-    res <- t(res)
-    colnames(res) <- rev(fas)
-
-    return(res)
-}
-
-
 LikelihoodData <- function(D) {
-
+    # Alternative likelihood for the data. Not presented in report
     Likelihood <- function(p) {
 
         succ.probs <- list(1-p, p)
@@ -69,28 +53,15 @@ LikelihoodData <- function(D) {
     return(Likelihood)
 }
 
-ProcessLik <- function(D) {
-
-    Lik <- function(p) {
-        probs <- rep(0, length(D))
-        probs[1] <- D[1]*p[1]
-        for (i in 2:length(D)) {
-            probs[i] <- D[i]*p[i]*prod(1-p[1:(i-1)])
-        }
-        return(probs)
-    }
-
-    return(Lik)
-}
-
-
-ProposalFunc <- function(p) {
-    return(sapply(p, function(x) { rtruncnorm(1, 0, 1, mean=x, sd=0.1) }))
-
-}
-
 mcmc <- function(ip, n.steps) {
     # MCMC sampling from the posterior of model parameters
+    # using the multinomial Likelihood for the entire process
+
+    ProposalFunc <- function(p) {
+        return(sapply(p, function(x) { rtruncnorm(1, 0, 1, mean=x, sd=0.1) }))
+
+    }
+
     data <- FilterControls()
     n.data <- rev(colSums(MakeAbsNums(data[1, ])))
 
@@ -100,7 +71,7 @@ mcmc <- function(ip, n.steps) {
     oldll <- dmultinom(n.data, sum(n.data), GetSinkProb(p), log=TRUE)
     for (i in 2:n.steps) {
         pp <- ProposalFunc(p)
-        loglik <- dmultinom(n.data, sum(n.data), GetSinkProb(pp), log=TRUE)#Likelihood(pp)
+        loglik <- dmultinom(n.data, sum(n.data), GetSinkProb(pp), log=TRUE)
         a <- loglik - oldll
         if (log(runif(1)) < a) {
             p <- pp
@@ -112,8 +83,6 @@ mcmc <- function(ip, n.steps) {
     return(mcmc.trace)
 }
 
-
-
 GetEventProb <- function(p, i) {
     if (i == 1) { return(p[1]) }
     pi <- p[i]*prod((1-p)[1:(i-1)])
@@ -124,52 +93,30 @@ GetSinkProb <- function(p) {
     return(sapply(1:length(p), function(x) { GetEventProb(p, x) }))
 }
 
-
-GetInputRateEstimates <- function() {
-    bern.tca <- 0.5
+GetStepsExp <- function() {
     data <- FilterControls()
     p <- rev(GetMLEstimates(data))
     ep <- GetSinkProb(p)
     event.exp <- sum((1:length(p)) * ep)
 
-    rates <- c(1, event.exp, event.exp)
-    rates <- rates/sum(rates)
-
-    rates[3] <- rates[3] / bern.tca
-    rates <- rates / sum(rates)
-
-    return(rates)
+    return(event.exp)
 }
 
 
-BinLik <- function(x, p, n) {
-    return(dbinom(x,size=n,prob=p,log=TRUE))
+CalcMLEClusters <- function() {
+    cl.inds <- lapply(1:5, function(x) { read.table(paste0("cl", x, ".txt")) })
+    fas <- c("C.12.0", "C.14.0", "C.16.0", "C.18.0", "C.20.0", "C22.0")
+    fname <- "data/GCFIDrawdata.csv"
+    data <- read.csv(fname)
+    data <- data[, rev(fas)]
+
+    data.cl <- lapply(cl.inds, function(ind.l) { data[unlist(ind.l), ] })
+    cl.centres <- lapply(data.cl, function(cl) { colMeans(cl) })
+    cl.centres[[4]] <- NULL
+    ml.est <- lapply(cl.centres, function(cl) {GetMLEstimates(t(as.matrix(cl)))})
+    ml.est.mat <- matrix(unlist(ml.est), nrow=4, byrow = T)
+    colnames(ml.est.mat) <- rev(fas)
+    ml.est.mat <- ml.est.mat[, fas]
+
+    return(ml.est.mat)
 }
-
-
-
-
-
-
-
-
-
-
-cl.inds <- lapply(1:5, function(x) { read.table(paste0("cl", x, ".txt")) })
-fas <- c("C.12.0", "C.14.0", "C.16.0", "C.18.0", "C.20.0", "C22.0")
-fname <- "data/GCFIDrawdata.csv"
-data <- read.csv(fname)
-data <- data[, rev(fas)]
-
-data.cl <- lapply(cl.inds, function(ind.l) { data[unlist(ind.l), ] })
-cl.centres <- lapply(data.cl, function(cl) { colMeans(cl) })
-cl.centres[[4]] <- NULL
-ml.est <- lapply(cl.centres, function(cl) {GetMLEstimates(t(as.matrix(cl)))})
-ml.est.mat <- matrix(unlist(ml.est), nrow=4, byrow = T)
-colnames(ml.est.mat) <- rev(fas)
-ml.est.mat <- ml.est.mat[, fas]
-
-xtable(ml.est.mat)
-
-
-
